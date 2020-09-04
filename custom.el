86c1c3448b97441fe70bc1bdbc0ccf919738d65e
Donald Brady <donald.brady@gmail.com>
2020-09-04 13:31:56 -0700

    minor tweaks

--- custom.el
+++ custom.el
@@ -4,9 +4,11 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(org-agenda-files
-   '("~/OrgDocuments/Books/Leading Lean/LeadingLean.org" "~/OrgDocuments/Books/ShapeUp/ShapeUp.org" "~/OrgDocuments/GitNotes/GitNotes.org" "~/OrgDocuments/python/python.org" "~/OrgDocuments/2020-02.org" "~/OrgDocuments/2020-03.org" "~/OrgDocuments/2020-04.org" "~/OrgDocuments/2020-05.org" "~/OrgDocuments/2020-06.org" "~/OrgDocuments/2020-07.org" "~/OrgDocuments/2020-08.org" "~/OrgDocuments/HugoNotes.org" "~/OrgDocuments/bsd405.org" "~/OrgDocuments/clock-table.org" "~/OrgDocuments/condo.org" "~/OrgDocuments/emacs.org" "~/OrgDocuments/goal-setting.org" "~/OrgDocuments/governance-models.org" "~/OrgDocuments/ideas.org" "~/OrgDocuments/index.org" "~/OrgDocuments/linode.org" "~/OrgDocuments/linux.org" "~/OrgDocuments/ml.org" "~/OrgDocuments/mysql.org" "~/OrgDocuments/ocean_letter.org" "~/OrgDocuments/org-mode.org" "~/OrgDocuments/quotes.org" "~/OrgDocuments/rss-feeds.org" "~/OrgDocuments/someday.org" "~/OrgDocuments/windows.org" "~/OrgDocuments/xr.org"))
+   (quote
+    ("~/OrgDocuments/Books/Leading Lean/LeadingLean.org" "~/OrgDocuments/Books/ShapeUp/ShapeUp.org" "~/OrgDocuments/GitNotes/GitNotes.org" "~/OrgDocuments/python/python.org" "~/OrgDocuments/2020-02.org" "~/OrgDocuments/2020-03.org" "~/OrgDocuments/2020-04.org" "~/OrgDocuments/2020-05.org" "~/OrgDocuments/2020-06.org" "~/OrgDocuments/2020-07.org" "~/OrgDocuments/2020-08.org" "~/OrgDocuments/HugoNotes.org" "~/OrgDocuments/bsd405.org" "~/OrgDocuments/clock-table.org" "~/OrgDocuments/condo.org" "~/OrgDocuments/emacs.org" "~/OrgDocuments/goal-setting.org" "~/OrgDocuments/governance-models.org" "~/OrgDocuments/ideas.org" "~/OrgDocuments/index.org" "~/OrgDocuments/linode.org" "~/OrgDocuments/linux.org" "~/OrgDocuments/ml.org" "~/OrgDocuments/mysql.org" "~/OrgDocuments/ocean_letter.org" "~/OrgDocuments/org-mode.org" "~/OrgDocuments/quotes.org" "~/OrgDocuments/rss-feeds.org" "~/OrgDocuments/someday.org" "~/OrgDocuments/windows.org" "~/OrgDocuments/xr.org")))
  '(package-selected-packages
-   '(magit org-protocol org-roam emacsql-sqlite3 easy-hugo elfeed-org elfeed htmlize gnuplot default-text-scale swiper ivy auto-compile use-package)))
+   (quote
+    (magit org-protocol org-roam emacsql-sqlite3 easy-hugo elfeed-org elfeed htmlize gnuplot default-text-scale swiper ivy auto-compile use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- lisp/deloitte.el
+++ lisp/deloitte.el
@@ -1,8 +1,12 @@
 ;; some useful work related functions
 
-;; Deloitte fiscal years begin on the first Monday of June
-;; e.g. FY20 began on 6/3/19
-;; e.g. FY21 began on 6/1/20
+;; Deloitte fiscal years begin on the Sunday before the first Monday of June
+;; e.g. FY20 began on 6/2/20
+;; e.g. FY21 began on 5/31/21
+
+;; | P1      | P2      | P3      | P4      | P5       |
+;; | 5/31/20 | 6/28/20 | 7/26/20 | 8/23/20 | 9/20/20  |
+;; | 6/27/20 | 7/25/20 | 8/22/20 | 9/19/20 | 10/17/20 |
 
 (require 'ts)
 
@@ -24,8 +28,8 @@
   (while (not (string-equal (ts-day-abbr june-walk) "Mon"))
     (setq june-walk (ts-adjust 'day 1 june-walk)))
 
-  ;; that's our day
-  june-walk)
+  ;; that's our day, now return the day (Sunday) before
+  (ts-adjust 'day -1 june-walk))
 
 
 (defun deloitte-start-of-current-fiscal-year ()
@@ -35,18 +39,18 @@
 
 (defun deloitte-current-fiscal-year-abbrev ()
   "Returns the standard fiscal year abbreviation e.f. FY21"
-  (s-concat "FY" (car (last (s-split "/" (deloitte-start-of-current-fiscal-year))))))
-
+  (setq yy-string (car (last (s-split "/" (deloitte-start-of-current-fiscal-year)))))
+  (s-concat "FY" (number-to-string (+ (string-to-number yy-string) 1))))
 
 (defun deloitte-financial-period ()
   "Returns the deloitte financial period which is divided into 13 four week periods"
   (interactive)
   ;; start at the begining of the year
-  (setq period 1)
+  (setq period 0)
   (setq period-walker (deloitte-ts-start-of-current-fiscal-year))
 
-  ;; walk forward in 4 week periods until >= today ??
-  (while (ts> (ts-now) period-walker)
+  ;; walk forward in 4 week periods until >= today
+  (while (ts>= (ts-now) period-walker)
     (setq period-walker (ts-adjust 'day 28 period-walker))
     (setq period (+ period 1)))
 
