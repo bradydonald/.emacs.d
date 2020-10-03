70f66e1ba1f4cf6e5c1a8bd9214534ea953957c3
Donald Brady <donald.brady@gmail.com>
2020-10-03 10:13:05 -0700

    Playing with org-edna

--- custom.el
+++ custom.el
@@ -3,10 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-agenda-files
-   '("~/OrgDocuments/Books/Leading Lean/LeadingLean.org" "~/OrgDocuments/Books/ShapeUp/ShapeUp.org" "~/OrgDocuments/GitNotes/GitNotes.org" "~/OrgDocuments/mandarin/mandarin1.org" "~/OrgDocuments/python/python.org" "~/OrgDocuments/2020-02.org" "~/OrgDocuments/2020-03.org" "~/OrgDocuments/2020-04.org" "~/OrgDocuments/2020-05.org" "~/OrgDocuments/2020-06.org" "~/OrgDocuments/2020-07.org" "~/OrgDocuments/2020-08.org" "~/OrgDocuments/2020-09.org" "~/OrgDocuments/HugoNotes.org" "~/OrgDocuments/bsd405.org" "~/OrgDocuments/clock-table.org" "~/OrgDocuments/condo.org" "~/OrgDocuments/emacs.org" "~/OrgDocuments/goal-setting.org" "~/OrgDocuments/governance-models.org" "~/OrgDocuments/ideas.org" "~/OrgDocuments/index.org" "~/OrgDocuments/linode.org" "~/OrgDocuments/linux.org" "~/OrgDocuments/ml.org" "~/OrgDocuments/mysql.org" "~/OrgDocuments/ocean_letter.org" "~/OrgDocuments/org-mode.org" "~/OrgDocuments/presentation.org" "~/OrgDocuments/quotes.org" "~/OrgDocuments/rss-feeds.org" "~/OrgDocuments/sap-preso.org" "~/OrgDocuments/someday.org" "~/OrgDocuments/windows.org" "~/OrgDocuments/xr-preso.org" "~/OrgDocuments/xr.org"))
  '(package-selected-packages
-   '(lsp-pyright magit org-protocol org-roam emacsql-sqlite3 easy-hugo elfeed-org elfeed htmlize gnuplot default-text-scale swiper ivy auto-compile use-package)))
+   '(org-edna exwm lsp-pyright magit org-protocol org-roam emacsql-sqlite3 easy-hugo elfeed-org elfeed htmlize gnuplot default-text-scale swiper ivy auto-compile use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
