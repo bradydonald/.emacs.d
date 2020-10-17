ab31055698574e28aea262de2c5f9f63fc63db66
Donald Brady <donald.brady@gmail.com>
2020-10-17 16:17:50 -0700

    mu4e configuration mostly.

--- custom.el
+++ custom.el
@@ -5,8 +5,10 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    '("79586dc4eb374231af28bbc36ba0880ed8e270249b07f814b0e6555bdcb71fab" default))
+ '(org-agenda-files
+   '("~/OrgDocuments/Books/Leading Lean/LeadingLean.org" "~/OrgDocuments/Books/ShapeUp/ShapeUp.org" "~/OrgDocuments/journals/2020-02.org" "~/OrgDocuments/journals/2020-03.org" "~/OrgDocuments/journals/2020-04.org" "~/OrgDocuments/journals/2020-05.org" "~/OrgDocuments/journals/2020-06.org" "~/OrgDocuments/journals/2020-07.org" "~/OrgDocuments/journals/2020-08.org" "~/OrgDocuments/journals/2020-09.org" "~/OrgDocuments/journals/2020-10.org" "~/OrgDocuments/mandarin/mandarin1.org" "~/OrgDocuments/python/python.org" "~/OrgDocuments/HugoNotes.org" "~/OrgDocuments/big-idea.org" "~/OrgDocuments/bsd405.org" "~/OrgDocuments/clock-table.org" "~/OrgDocuments/condo.org" "~/OrgDocuments/emacs.org" "~/OrgDocuments/goal-setting.org" "~/OrgDocuments/governance-models.org" "~/OrgDocuments/hostwinds.org" "~/OrgDocuments/ideas.org" "~/OrgDocuments/index.org" "~/OrgDocuments/linode.org" "~/OrgDocuments/linux.org" "~/OrgDocuments/ml.org" "~/OrgDocuments/mysql.org" "~/OrgDocuments/ocean_letter.org" "~/OrgDocuments/org-mode.org" "~/OrgDocuments/presentation.org" "~/OrgDocuments/quotes.org" "~/OrgDocuments/rss-feeds.org" "~/OrgDocuments/sap-preso.org" "~/OrgDocuments/someday.org" "~/OrgDocuments/web-articles.org" "~/OrgDocuments/windows.org" "~/OrgDocuments/xr-preso.org"))
  '(package-selected-packages
-   '(darcula-theme org-edna exwm lsp-pyright magit org-protocol org-roam emacsql-sqlite3 easy-hugo elfeed-org elfeed htmlize gnuplot default-text-scale swiper ivy auto-compile use-package)))
+   '(org-plus-contrib darcula-theme org-edna exwm lsp-pyright magit org-protocol org-roam emacsql-sqlite3 easy-hugo elfeed-org elfeed htmlize gnuplot default-text-scale swiper ivy auto-compile use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
