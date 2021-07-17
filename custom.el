687e88157c69f90d9d58d642116045dc03da1754
Donald Brady <donald.brady@gmail.com>
2021-07-17 07:26:03 -0700

    fiddling with org-roam v2

--- custom.el
+++ custom.el
@@ -4,4 +4,12 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("33ea268218b70aa106ba51a85fe976bfae9cf6931b18ceaf57159c558bbcd1e6" default)))
+   '("a1d906942ff0776ce2dd72cd731126a35dc3d3140a765a0bb5a1a1b6a16c6b45" "33ea268218b70aa106ba51a85fe976bfae9cf6931b18ceaf57159c558bbcd1e6" default))
+ '(package-selected-packages
+   '(weyland-yutani-theme s gnuplot git-timemachine flycheck elec-pair eglot)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
