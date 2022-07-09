15d2e098aeadad6852d1acf6469fe7ffe50c5292
Donald Brady <donald.brady@gmail.com>
2022-07-09 08:29:25 -0700

    cleanup dupe function

--- lisp/misc.el
+++ lisp/misc.el
@@ -4,15 +4,6 @@
     (print (car list))
     (setq list (cdr list))))
 
-(defun db/toggle-transparency ()
-  "Toggles transparency of the frame for note taking on zoom etc"
-  (interactive)
-  (setq current-alpha-value (cadr (frame-parameter (selected-frame) 'alpha)))
-  (if (equal nil current-alpha-value)
-      (setq current-alpha-value 100))
-   (if (/= current-alpha-value 100)
-       (set-frame-parameter nil 'alpha '(100 100))
-     (set-frame-parameter nil 'alpha '(85 50))))
 
 ;;
 ;; Credit to Xah
