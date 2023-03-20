75072c5e96a9b3de4d82352677c40c99356249bd
Donald Brady <donald.brady@gmail.com>
2023-03-20 16:55:43 -0700

    update 13 packages

---
+++ elpa/mastodon-20230320.2110/mastodon-pkg.el
@@ -0,0 +1,14 @@
+(define-package "mastodon" "20230320.2110" "Client for Mastodon, a federated social network"
+  '((emacs "27.1")
+    (request "0.3.0")
+    (persist "0.4")
+    (ts "0.3"))
+  :commit "4eed47ea1fec9cdbcacfddd981770acecbb345c4" :authors
+  '(("Johnson Denen" . "johnson.denen@gmail.com")
+    ("Marty Hiatt" . "martianhiatus@riseup.net"))
+  :maintainer
+  '("Marty Hiatt" . "martianhiatus@riseup.net")
+  :url "https://codeberg.org/martianh/mastodon.el")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/posframe-20230320.740/posframe-pkg.el
@@ -0,0 +1,12 @@
+(define-package "posframe" "20230320.740" "Pop a posframe (just a frame) at point"
+  '((emacs "26.1"))
+  :commit "962520d5da6f70dbaf350590178b6ea838908752" :authors
+  '(("Feng Shu" . "tumashu@163.com"))
+  :maintainer
+  '("Feng Shu" . "tumashu@163.com")
+  :keywords
+  '("convenience" "tooltip")
+  :url "https://github.com/tumashu/posframe")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
