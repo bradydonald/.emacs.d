c872f7fb9f74bc49bad6abd9e6f21ecb16f41f9d
Donald Brady <donald.brady@gmail.com>
2023-03-29 09:23:09 -0700

    package update

--- elpa/mastodon-20230328.1252/mastodon-pkg.el
+++ elpa/mastodon-20230328.1252/mastodon-pkg.el
@@ -1,9 +1,9 @@
-(define-package "mastodon" "20230327.1856" "Client for Mastodon, a federated social network"
+(define-package "mastodon" "20230328.1252" "Client for Mastodon, a federated social network"
   '((emacs "27.1")
     (request "0.3.0")
     (persist "0.4")
     (ts "0.3"))
-  :commit "557c7f938ba3326e9051c837ad9aa33cc1d13169" :authors
+  :commit "14dbf79135f6751fc93eb5e85e0b2eae3bde6568" :authors
   '(("Johnson Denen" . "johnson.denen@gmail.com")
     ("Marty Hiatt" . "martianhiatus@riseup.net"))
   :maintainer
