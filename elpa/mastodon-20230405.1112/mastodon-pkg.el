3ff45806d2a72b9b1c2e795a7535ae3225e137dc
Donald Brady <donald.brady@gmail.com>
2023-04-06 12:19:05 -0400

    updates and theme change

--- elpa/mastodon-20230405.1112/mastodon-pkg.el
+++ elpa/mastodon-20230405.1112/mastodon-pkg.el
@@ -1,9 +1,9 @@
-(define-package "mastodon" "20230405.931" "Client for Mastodon and compatible fediverse services"
+(define-package "mastodon" "20230405.1112" "Client for Mastodon and compatible fediverse services"
   '((emacs "27.1")
     (request "0.3.0")
     (persist "0.4")
     (ts "0.3"))
-  :commit "ccf2d8d5cdf1655888292c2ce7dd45c86de4e805" :authors
+  :commit "40a2c91fa7c34be09c2527daf01b9deee7e87f2a" :authors
   '(("Johnson Denen" . "johnson.denen@gmail.com")
     ("Marty Hiatt" . "martianhiatus@riseup.net"))
   :maintainer
