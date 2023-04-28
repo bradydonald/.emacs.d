605b9dbc75cb91dda2d7e7d0342f9427ee77fdd3
Donald Brady <donald.brady@gmail.com>
2023-04-28 07:16:25 -0700

    upgrade 4

--- elpa/magit-20230428.1246/magit-pkg.el
+++ elpa/magit-20230428.1246/magit-pkg.el
@@ -1,4 +1,4 @@
-(define-package "magit" "20230425.2031" "A Git porcelain inside Emacs."
+(define-package "magit" "20230428.1246" "A Git porcelain inside Emacs."
   '((emacs "25.1")
     (compat "29.1.3.4")
     (dash "20221013")
@@ -6,7 +6,7 @@
     (magit-section "20230101")
     (transient "20230201")
     (with-editor "20230118"))
-  :commit "94009b0407853ddd4c1d1f147d0c3089a557c035" :authors
+  :commit "654b8ceff31f3a0401d20c64fdb69b042bb1c81e" :authors
   '(("Marius Vollmer" . "marius.vollmer@gmail.com")
     ("Jonas Bernoulli" . "jonas@bernoul.li"))
   :maintainer
--- elpa/mastodon-20230428.1327/mastodon-pkg.el
+++ elpa/mastodon-20230428.1327/mastodon-pkg.el
@@ -1,11 +1,13 @@
-(define-package "mastodon" "20230425.742" "Client for fediverse services that implement the Mastodon API"
+(define-package "mastodon" "20230428.1327" "Client for fediverse services that implement the Mastodon API"
   '((emacs "27.1")
     (request "0.3.0")
     (persist "0.4")
     (ts "0.3"))
-  :commit "d2aea126a9d5a5873a835a8ed390956c481c587c" :authors
+  :commit "d841c2522ea63e5a2e02811b20506185072740ca" :authors
   '(("Johnson Denen" . "johnson.denen@gmail.com")
     ("Marty Hiatt" . "martianhiatus@riseup.net"))
+  :maintainers
+  '(("Marty Hiatt" . "martianhiatus@riseup.net"))
   :maintainer
   '("Marty Hiatt" . "martianhiatus@riseup.net")
   :url "https://codeberg.org/martianh/mastodon.el")
