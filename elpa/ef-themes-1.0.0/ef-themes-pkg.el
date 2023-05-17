6cb287f44fe8c440db448a5477010aad2706d12e
Donald Brady <donald.brady@gmail.com>
2023-05-17 12:06:49 -0700

    update 5

---
+++ elpa/ef-themes-1.0.0/ef-themes-pkg.el
@@ -0,0 +1,2 @@
+;; Generated package description from ef-themes.el  -*- no-byte-compile: t -*-
+(define-package "ef-themes" "1.0.0" "Colorful and legible themes" '((emacs "27.1")) :commit "27c8a119557ddd183a9b679c01e65a70b5c79636" :authors '(("Protesilaos Stavrou" . "info@protesilaos.com")) :maintainer '("Ef-Themes Development" . "~protesilaos/ef-themes@lists.sr.ht") :keywords '("faces" "theme" "accessibility") :url "https://git.sr.ht/~protesilaos/ef-themes")
