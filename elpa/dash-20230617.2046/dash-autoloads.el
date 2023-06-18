d52f89c9cb35fb6053cd22bd03206e4e8e3e99db
Donald Brady <donald.brady@gmail.com>
2023-06-18 07:02:11 -0700

    upgrade 1

--- elpa/dash-20230617.2046/dash-autoloads.el
+++ elpa/dash-20230617.2046/dash-autoloads.el
@@ -5,8 +5,7 @@
 
 ;;; Code:
 
-(add-to-list 'load-path (directory-file-name
-                         (or (file-name-directory #$) (car load-path))))
+(add-to-list 'load-path (or (and load-file-name (directory-file-name (file-name-directory load-file-name))) (car load-path)))
 
 
 
@@ -39,7 +38,7 @@ evaluate `dash-fontify-mode'.
 The mode's hook is called both when the mode is enabled and when
 it is disabled.
 
-(fn &optional ARG)" t nil)
+(fn &optional ARG)" t)
 (put 'global-dash-fontify-mode 'globalized-minor-mode t)
 (defvar global-dash-fontify-mode nil "\
 Non-nil if Global Dash-Fontify mode is enabled.
@@ -51,22 +50,22 @@ or call the function `global-dash-fontify-mode'.")
 (custom-autoload 'global-dash-fontify-mode "dash" nil)
 (autoload 'global-dash-fontify-mode "dash" "\
 Toggle Dash-Fontify mode in all buffers.
-With prefix ARG, enable Global Dash-Fontify mode if ARG is positive;
-otherwise, disable it.
+With prefix ARG, enable Global Dash-Fontify mode if ARG is positive; otherwise,
+disable it.
 
 If called from Lisp, toggle the mode if ARG is `toggle'.
 Enable the mode if ARG is nil, omitted, or is a positive number.
 Disable the mode if ARG is a negative number.
 
-Dash-Fontify mode is enabled in all buffers where
-`dash--turn-on-fontify-mode' would do it.
+Dash-Fontify mode is enabled in all buffers where `dash--turn-on-fontify-mode'
+would do it.
 
 See `dash-fontify-mode' for more information on Dash-Fontify mode.
 
-(fn &optional ARG)" t nil)
+(fn &optional ARG)" t)
 (autoload 'dash-register-info-lookup "dash" "\
 Register the Dash Info manual with `info-lookup-symbol'.
-This allows Dash symbols to be looked up with \\[info-lookup-symbol]." t nil)
+This allows Dash symbols to be looked up with \\[info-lookup-symbol]." t)
 (register-definition-prefixes "dash" '("!cdr" "!cons" "--" "->" "-a" "-butlast" "-c" "-d" "-e" "-f" "-gr" "-i" "-juxt" "-keep" "-l" "-m" "-no" "-o" "-p" "-r" "-s" "-t" "-u" "-value-to-list" "-when-let" "-zip" "dash-"))
 
 ;;; End of scraped data
@@ -77,6 +76,7 @@ This allows Dash symbols to be looked up with \\[info-lookup-symbol]." t nil)
 ;; version-control: never
 ;; no-byte-compile: t
 ;; no-update-autoloads: t
+;; no-native-compile: t
 ;; coding: utf-8-emacs-unix
 ;; End:
 
