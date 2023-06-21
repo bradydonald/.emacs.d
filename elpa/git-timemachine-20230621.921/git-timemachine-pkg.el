bb2335c50d286749bde35e957e7e15efb8eb195c
Donald Brady <donald.brady@gmail.com>
2023-06-21 12:20:13 -0700

    update packages

---
+++ elpa/git-timemachine-20230621.921/git-timemachine-pkg.el
@@ -0,0 +1,15 @@
+(define-package "git-timemachine" "20230621.921" "Walk through git revisions of a file"
+  '((emacs "24.3")
+    (transient "0.1.0"))
+  :commit "bc8c7d7c22aeeced2510023b2e6c5d8ce8c574aa" :authors
+  '(("Peter Stiernström" . "peter@stiernstrom.se"))
+  :maintainers
+  '(("Peter Stiernström" . "peter@stiernstrom.se"))
+  :maintainer
+  '("Peter Stiernström" . "peter@stiernstrom.se")
+  :keywords
+  '("vc")
+  :url "https://gitlab.com/pidu/git-timemachine")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
