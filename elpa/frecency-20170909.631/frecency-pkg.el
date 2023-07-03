48059c415a232cf83924523e1b50f0e6113b4daf
Donald Brady <donald.brady@gmail.com>
2023-07-03 16:52:54 -0700

    package deltas

---
+++ elpa/frecency-20170909.631/frecency-pkg.el
@@ -0,0 +1,16 @@
+(define-package "frecency" "20170909.631" "Library for sorting items by frequency and recency of access"
+  '((emacs "25.1")
+    (a "0.1")
+    (dash "2.13.0"))
+  :commit "132130088ef5695cffed6fcacfa219cb0c389026" :authors
+  '(("Adam Porter" . "adam@alphapapa.net"))
+  :maintainers
+  '(("Adam Porter" . "adam@alphapapa.net"))
+  :maintainer
+  '("Adam Porter" . "adam@alphapapa.net")
+  :keywords
+  '("libraries" "recency" "recent" "frequency" "frequent")
+  :url "http://github.com/alphapapa/frecency.el")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
