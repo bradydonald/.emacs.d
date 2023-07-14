63f670f54e1b54d5069e622fddfc7891889a0651
Donald Brady <donald.brady@gmail.com>
2023-07-14 14:49:53 -0700

    upgrade 12

---
+++ elpa/mastodon-20230713.835/mastodon-pkg.el
@@ -0,0 +1,16 @@
+(define-package "mastodon" "20230713.835" "Client for fediverse services using the Mastodon API"
+  '((emacs "27.1")
+    (request "0.3.0")
+    (persist "0.4")
+    (ts "0.3"))
+  :commit "5123212fa191ce5215262367d1022fd1441dc19a" :authors
+  '(("Johnson Denen" . "johnson.denen@gmail.com")
+    ("Marty Hiatt" . "martianhiatus@riseup.net"))
+  :maintainers
+  '(("Marty Hiatt" . "martianhiatus@riseup.net"))
+  :maintainer
+  '("Marty Hiatt" . "martianhiatus@riseup.net")
+  :url "https://codeberg.org/martianh/mastodon.el")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
