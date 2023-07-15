81aadb1d8ef4fb89fcf678ea38e337d06428a135
Donald Brady <donald.brady@gmail.com>
2023-07-15 13:56:11 -0700

    chatgpt-shell

---
+++ elpa/chatgpt-shell-20230715.1529/chatgpt-shell-pkg.el
@@ -0,0 +1,13 @@
+(define-package "chatgpt-shell" "20230715.1529" "ChatGPT shell + buffer insert commands"
+  '((emacs "27.1")
+    (shell-maker "0.42.1"))
+  :commit "a4efa0990a1423bbd61880e298d981f71d516a6c" :authors
+  '(("Alvaro Ramirez https://xenodium.com"))
+  :maintainers
+  '(("Alvaro Ramirez https://xenodium.com"))
+  :maintainer
+  '("Alvaro Ramirez https://xenodium.com")
+  :url "https://github.com/xenodium/chatgpt-shell")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
