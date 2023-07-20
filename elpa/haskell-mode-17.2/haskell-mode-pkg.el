80e25454c757a736c991cebae4be8623c883a5ea
Donald Brady <donald.brady@gmail.com>
2023-07-20 06:30:56 -0700

    update 12

--- elpa/haskell-mode-17.2/haskell-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;; Generated package description from haskell-mode.el  -*- no-byte-compile: t -*-
-(define-package "haskell-mode" "17.2" "A Haskell editing mode" 'nil :commit "e72677668f5fc7cc148008e885a0f256e245dd43" :authors '(("1992      Simon Marlow") ("1997-1998 Graeme E Moss" . "gem@cs.york.ac.uk") ("Tommy Thorn" . "thorn@irisa.fr") ("2001-2002 Reuben Thomas (>=v1.4)") ("2003      Dave Love" . "fx@gnu.org") ("2016      Arthur Fayzrakhmanov")) :maintainer '("1992      Simon Marlow") :keywords '("faces" "files" "haskell") :url "https://github.com/haskell/haskell-mode")
