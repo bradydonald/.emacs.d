4014f7d131993e9db53b1a39d2bab58cbce6b0ad
Donald Brady <donald.brady@gmail.com>
2023-09-29 11:38:35 -0700

    add all-the-icons[-dired]

---
+++ elpa/dired-hacks-utils-20230512.1107/dired-hacks-utils-autoloads.el
@@ -0,0 +1,10 @@
+(add-to-list 'load-path (or (and load-file-name (directory-file-name (file-name-directory load-file-name))) (car load-path)))
+
+
+
+;;; Generated autoloads from dired-hacks-utils.el
+
+(register-definition-prefixes "dired-hacks-utils" '("dired-"))
+
+;;; End of scraped data
+
