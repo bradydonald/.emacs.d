80133402b18fd13314f72de131ab2cea9bc8e01f
Donald Brady <donald.brady@gmail.com>
2023-10-22 06:40:36 -0700

    upgrade 6

---
+++ elpa/lem-20231022.1228/lem-pkg.el
@@ -0,0 +1,16 @@
+(define-package "lem" "20231022.1228" "A basic lemmy client"
+  '((emacs "29.1")
+    (fedi "0.1")
+    (markdown-mode "2.5"))
+  :commit "1aeaf39e79940c5f684c43cf7219279ce818c2d3" :authors
+  '(("martian hiatus <martianhiatus [a t] riseup [d o t] net>"))
+  :maintainers
+  '(("martian hiatus <martianhiatus [a t] riseup [d o t] net>"))
+  :maintainer
+  '("martian hiatus <martianhiatus [a t] riseup [d o t] net>")
+  :keywords
+  '("multimedia" "comm" "web" "fediverse")
+  :url "https://codeberg.org/martianh/lem.el")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
