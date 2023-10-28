f7ed7202b902a48b6dd76b78d77b55b9d13e2f6a
Donald Brady <donald.brady@gmail.com>
2023-10-28 06:25:21 -0700

    +mpv support

---
+++ elpa/mpv-0.2.0/mpv-pkg.el
@@ -0,0 +1,17 @@
+(define-package "mpv" "0.2.0" "control mpv for easy note-taking"
+  '((cl-lib "0.5")
+    (emacs "25.1")
+    (json "1.3")
+    (org "8.0"))
+  :commit "4fd8baa508dbc1a6b42b4e40292c0dbb0f19c9b9" :authors
+  '(("Johann Klähn" . "johann@jklaehn.de"))
+  :maintainers
+  '(("Johann Klähn" . "johann@jklaehn.de"))
+  :maintainer
+  '("Johann Klähn" . "johann@jklaehn.de")
+  :keywords
+  '("tools" "multimedia")
+  :url "https://github.com/kljohann/mpv.el")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
