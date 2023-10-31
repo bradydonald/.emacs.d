310c2f0b597ff7132f482b9ac77b2c1196945dbf
Donald Brady <donald.brady@gmail.com>
2023-10-31 14:37:37 -0700

    upgrade 8

---
+++ elpa/empv-20231007.1911/empv-pkg.el
@@ -0,0 +1,13 @@
+(define-package "empv" "20231007.1911" "A multimedia player/manager, YouTube interface"
+  '((emacs "28.1")
+    (s "1.13.0"))
+  :commit "4af3b3624a4714558081d693c91602b9304e8fcb" :authors
+  '(("Isa Mert Gurbuz" . "isamertgurbuz@gmail.com"))
+  :maintainers
+  '(("Isa Mert Gurbuz" . "isamertgurbuz@gmail.com"))
+  :maintainer
+  '("Isa Mert Gurbuz" . "isamertgurbuz@gmail.com")
+  :url "https://github.com/isamert/empv.el")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
