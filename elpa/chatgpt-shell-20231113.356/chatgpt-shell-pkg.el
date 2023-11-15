65dfe0f01263e89f2b68ac9430fd1299afacf9f6
Donald Brady <donald.brady@gmail.com>
2023-11-15 09:55:58 +0000

    upgrade 8

---
+++ elpa/chatgpt-shell-20231113.356/chatgpt-shell-pkg.el
@@ -0,0 +1,13 @@
+(define-package "chatgpt-shell" "20231113.356" "ChatGPT shell + buffer insert commands"
+  '((emacs "27.1")
+    (shell-maker "0.43.1"))
+  :commit "50984ba1e833be89c41b9779e8dca5d04cb16a29" :authors
+  '(("Alvaro Ramirez https://xenodium.com"))
+  :maintainers
+  '(("Alvaro Ramirez https://xenodium.com"))
+  :maintainer
+  '("Alvaro Ramirez https://xenodium.com")
+  :url "https://github.com/xenodium/chatgpt-shell")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/fedi-20231112.911/fedi-pkg.el
@@ -0,0 +1,13 @@
+(define-package "fedi" "20231112.911" "Helper functions for fediverse clients"
+  '((emacs "28.1")
+    (markdown-mode "2.5"))
+  :commit "e606fd40de36ad2131d027dec4f254e59a120dd5" :authors
+  '(("Marty Hiatt" . "martianhiatus@riseup.net"))
+  :maintainers
+  '(("Marty Hiatt" . "martianhiatus@riseup.net"))
+  :maintainer
+  '("Marty Hiatt" . "martianhiatus@riseup.net")
+  :url "https://codeberg.org/martianh/fedi.el")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
