9884377d9703b53d7eba6f2cb7225ae4fa9d570b
d <donaldbrady@Donalds-MacBook-Pro.local>
2024-01-05 13:32:35 -0800

    package upgrade

---
+++ elpa/jinx-20240104.1838/jinx-pkg.el
@@ -0,0 +1,15 @@
+(define-package "jinx" "20240104.1838" "Enchanted Spell Checker"
+  '((emacs "27.1")
+    (compat "29.1.4.4"))
+  :commit "d2caef25a0d7a3eaa55da43c928ec944e0d09450" :authors
+  '(("Daniel Mendler" . "mail@daniel-mendler.de"))
+  :maintainers
+  '(("Daniel Mendler" . "mail@daniel-mendler.de"))
+  :maintainer
+  '("Daniel Mendler" . "mail@daniel-mendler.de")
+  :keywords
+  '("convenience" "text")
+  :url "https://github.com/minad/jinx")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
