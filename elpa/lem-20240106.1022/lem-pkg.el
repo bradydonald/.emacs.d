fa4dfa540e3f005672d942310abdf18d0d2e00a1
d <donaldbrady@Donalds-MacBook-Pro.local>
2024-01-12 09:40:40 -0800

    upgrade 13

---
+++ elpa/lem-20240106.1022/lem-pkg.el
@@ -0,0 +1,16 @@
+(define-package "lem" "20240106.1022" "A basic lemmy client"
+  '((emacs "29.1")
+    (fedi "0.1")
+    (markdown-mode "2.5"))
+  :commit "3cdbdb1c1b677ad8337dfbe053f497e9ea3500a6" :authors
+  '(("martian hiatus <martianhiatus [a t] riseup [d o t] net>"))
+  :maintainers
+  '(("martian hiatus <martianhiatus [a t] riseup [d o t] net>"))
+  :maintainer
+  '("martian hiatus <martianhiatus [a t] riseup [d o t] net>")
+  :keywords
+  '("multimedia" "comm" "web" "fediverse")
+  :url "https://codeberg.org/martianh/lem.el")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/shell-maker-20240111.840/shell-maker-pkg.el
@@ -0,0 +1,12 @@
+(define-package "shell-maker" "20240111.840" "Interaction mode for making comint shells"
+  '((emacs "27.1"))
+  :commit "f0ea5ff835178d082a59495f1cc9d2146b2ee8ff" :authors
+  '(("Alvaro Ramirez https://xenodium.com"))
+  :maintainers
+  '(("Alvaro Ramirez https://xenodium.com"))
+  :maintainer
+  '("Alvaro Ramirez https://xenodium.com")
+  :url "https://github.com/xenodium/chatgpt-shell")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
