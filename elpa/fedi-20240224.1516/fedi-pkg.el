8a3e5b2ae2f0956c5a385bac6055044654214029
d <donaldbrady@Donalds-MacBook-Pro.local>
2024-02-25 13:35:06 -0800

    upgrade 14

---
+++ elpa/fedi-20240224.1516/fedi-pkg.el
@@ -0,0 +1,13 @@
+(define-package "fedi" "20240224.1516" "Helper functions for fediverse clients"
+  '((emacs "28.1")
+    (markdown-mode "2.5"))
+  :commit "2901af65ed4665bde776f7ba6bbd56fee9db7125" :authors
+  '(("Marty Hiatt" . "martianhiatus@riseup.net"))
+  :maintainers
+  '(("Marty Hiatt" . "martianhiatus@riseup.net"))
+  :maintainer
+  '("Marty Hiatt" . "martianhiatus@riseup.net")
+  :url "https://codeberg.org/martianh/fedi.el")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
