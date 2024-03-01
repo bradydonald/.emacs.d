99826b07b9c90d23488bfc1fa00cebfcb8c90d6f
d <donaldbrady@Donalds-MacBook-Pro.local>
2024-03-01 12:52:37 -0800

    upgrade 11

---
+++ elpa/chatgpt-shell-20240227.2313/chatgpt-shell-pkg.el
@@ -0,0 +1,13 @@
+(define-package "chatgpt-shell" "20240227.2313" "ChatGPT shell + buffer insert commands"
+  '((emacs "27.1")
+    (shell-maker "0.50.1"))
+  :commit "d09df22aa125c5c88dc7d363693f08ff18f490c9" :authors
+  '(("Alvaro Ramirez https://xenodium.com"))
+  :maintainers
+  '(("Alvaro Ramirez https://xenodium.com"))
+  :maintainer
+  '("Alvaro Ramirez https://xenodium.com")
+  :url "https://github.com/xenodium/chatgpt-shell")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
