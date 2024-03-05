388dbaf3e319e1a989529f14942c5e1bb48ab4b1
d <donaldbrady@Donalds-MacBook-Pro.local>
2024-03-04 17:03:18 -0800

    add gptel with ollama support for testing.

---
+++ elpa/gptel-20240304.1928/gptel-pkg.el
@@ -0,0 +1,16 @@
+(define-package "gptel" "20240304.1928" "Interact with ChatGPT or other LLMs"
+  '((emacs "27.1")
+    (transient "0.4.0")
+    (compat "29.1.4.1"))
+  :commit "eb088f2f21962a07224cd2b695fcee25b7180a5d" :authors
+  '(("Karthik Chikmagalur"))
+  :maintainers
+  '(("Karthik Chikmagalur"))
+  :maintainer
+  '("Karthik Chikmagalur")
+  :keywords
+  '("convenience")
+  :url "https://github.com/karthink/gptel")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
