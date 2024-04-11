c8c3ae6ad3b6cb25527c49e0a7fac152f767c5f2
d <donaldbrady@db-mbpro.local>
2024-04-11 16:50:29 -0700

    upgrade 26

---
+++ elpa/casual-20240405.1932/casual-pkg.el
@@ -0,0 +1,14 @@
+(define-package "casual" "20240405.1932" "Transient UI for Calc"
+  '((emacs "29.1"))
+  :commit "2d754a95f6d76ced2f4b191511b6078e8d020b9b" :authors
+  '(("Charles Choi" . "kickingvegas@gmail.com"))
+  :maintainers
+  '(("Charles Choi" . "kickingvegas@gmail.com"))
+  :maintainer
+  '("Charles Choi" . "kickingvegas@gmail.com")
+  :keywords
+  '("tools")
+  :url "https://github.com/kickingvegas/casual")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/gptel-20240410.1650/gptel-pkg.el
@@ -0,0 +1,16 @@
+(define-package "gptel" "20240410.1650" "Interact with ChatGPT or other LLMs"
+  '((emacs "27.1")
+    (transient "0.4.0")
+    (compat "29.1.4.1"))
+  :commit "70889ad95cb0a6bdb793a4e36b8bb6919b808a2e" :authors
+  '(("Karthik Chikmagalur"))
+  :maintainers
+  '(("Karthik Chikmagalur"))
+  :maintainer
+  '("Karthik Chikmagalur")
+  :keywords
+  '("convenience")
+  :url "https://github.com/karthink/gptel")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
