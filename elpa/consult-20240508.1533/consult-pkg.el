4dd6bd4191ebf411e9306fc3a66b408f3237e0fb
Donald Brady <164683436+donaldbrady3v@users.noreply.github.com>
2024-05-08 14:17:07 -0700

    upgrade 4

---
+++ elpa/consult-20240508.1533/consult-pkg.el
@@ -0,0 +1,15 @@
+(define-package "consult" "20240508.1533" "Consulting completing-read"
+  '((emacs "27.1")
+    (compat "29.1.4.4"))
+  :commit "369592fe2f160c1b54f91266104992fc25bc5e14" :authors
+  '(("Daniel Mendler and Consult contributors"))
+  :maintainers
+  '(("Daniel Mendler" . "mail@daniel-mendler.de"))
+  :maintainer
+  '("Daniel Mendler" . "mail@daniel-mendler.de")
+  :keywords
+  '("matching" "files" "completion")
+  :url "https://github.com/minad/consult")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/jinx-20240508.835/jinx-pkg.el
@@ -0,0 +1,15 @@
+(define-package "jinx" "20240508.835" "Enchanted Spell Checker"
+  '((emacs "27.1")
+    (compat "29.1.4.4"))
+  :commit "2a18f12769b18c84b0706a4c2f3256b5cf7689cc" :authors
+  '(("Daniel Mendler" . "mail@daniel-mendler.de"))
+  :maintainers
+  '(("Daniel Mendler" . "mail@daniel-mendler.de"))
+  :maintainer
+  '("Daniel Mendler" . "mail@daniel-mendler.de")
+  :keywords
+  '("convenience" "text")
+  :url "https://github.com/minad/jinx")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
