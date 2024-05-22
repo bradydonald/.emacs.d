f52bfa919c1f85d11d7e3873ecac0e61bf35c15d
Donald Brady <164683436+donaldbrady3v@users.noreply.github.com>
2024-05-22 12:47:56 +0300

    upgrade 9

---
+++ elpa/gptel-20240520.1943/gptel-pkg.el
@@ -0,0 +1,16 @@
+(define-package "gptel" "20240520.1943" "Interact with ChatGPT or other LLMs"
+  '((emacs "27.1")
+    (transient "0.4.0")
+    (compat "29.1.4.1"))
+  :commit "3bce2caa5dc773d1b1fce53e8453d2e1ce534b8b" :authors
+  '(("Karthik Chikmagalur" . "karthik.chikmagalur@gmail.com"))
+  :maintainers
+  '(("Karthik Chikmagalur" . "karthik.chikmagalur@gmail.com"))
+  :maintainer
+  '("Karthik Chikmagalur" . "karthik.chikmagalur@gmail.com")
+  :keywords
+  '("convenience")
+  :url "https://github.com/karthink/gptel")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
