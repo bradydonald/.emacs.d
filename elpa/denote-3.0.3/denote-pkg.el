5d3b0bec8846f44ae43407b068b83bc4b66a784d
Donald Brady <164683436+donaldbrady3v@users.noreply.github.com>
2024-07-04 12:49:26 -0700

    upgrade 20

---
+++ elpa/denote-3.0.3/denote-pkg.el
@@ -0,0 +1,2 @@
+;; Generated package description from denote.el  -*- no-byte-compile: t -*-
+(define-package "denote" "3.0.3" "Simple notes with an efficient file-naming scheme" '((emacs "28.1")) :commit "aaa9c837404f27a50242d481abae851084fe1634" :authors '(("Protesilaos Stavrou" . "info@protesilaos.com")) :maintainer '("Protesilaos Stavrou" . "info@protesilaos.com") :url "https://github.com/protesilaos/denote")
---
+++ elpa/lem-20240630.1228/lem-pkg.el
@@ -0,0 +1,16 @@
+(define-package "lem" "20240630.1228" "A lemmy client"
+  '((emacs "29.1")
+    (fedi "0.2")
+    (markdown-mode "2.5"))
+  :commit "2dc5036f0991db352948ea93ae895654c0fe775d" :authors
+  '(("martian hiatus <martianhiatus [a t] riseup [d o t] net>"))
+  :maintainers
+  '(("martian hiatus <martianhiatus [a t] riseup [d o t] net>"))
+  :maintainer
+  '("martian hiatus <martianhiatus [a t] riseup [d o t] net>")
+  :keywords
+  '("multimedia" "comm" "web" "fediverse")
+  :url "https://codeberg.org/martianh/lem.el")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
