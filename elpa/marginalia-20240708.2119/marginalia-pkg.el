517fa5f325e71456709ba5453bf9362c84be8b9c
Donald Brady <164683436+donaldbrady3v@users.noreply.github.com>
2024-07-08 16:39:05 -0700

    upgrade 8

---
+++ elpa/marginalia-20240708.2119/marginalia-pkg.el
@@ -0,0 +1,17 @@
+(define-package "marginalia" "20240708.2119" "Enrich existing commands with completion annotations"
+  '((emacs "27.1")
+    (compat "30"))
+  :commit "9f5360e94aecb27c95804836887fc9c8d4a5d85a" :authors
+  '(("Omar Antolín Camarena" . "omar@matem.unam.mx")
+    ("Daniel Mendler" . "mail@daniel-mendler.de"))
+  :maintainers
+  '(("Omar Antolín Camarena" . "omar@matem.unam.mx")
+    ("Daniel Mendler" . "mail@daniel-mendler.de"))
+  :maintainer
+  '("Omar Antolín Camarena" . "omar@matem.unam.mx")
+  :keywords
+  '("docs" "help" "matching" "completion")
+  :url "https://github.com/minad/marginalia")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
