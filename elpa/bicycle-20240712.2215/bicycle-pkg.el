dda67577af0513e98ca2b2dc8451c422c99a5353
Donald Brady <164683436+donaldbrady3v@users.noreply.github.com>
2024-07-13 18:49:35 -0700

    upgrade 11

---
+++ elpa/bicycle-20240712.2215/bicycle-pkg.el
@@ -0,0 +1,15 @@
+(define-package "bicycle" "20240712.2215" "Cycle outline and code visibility"
+  '((emacs "25.1")
+    (compat "30.0.0.0"))
+  :commit "0b7ea156d211e8a87a931afa210d4f6beea20f5d" :authors
+  '(("Jonas Bernoulli" . "emacs.bicycle@jonas.bernoulli.dev"))
+  :maintainers
+  '(("Jonas Bernoulli" . "emacs.bicycle@jonas.bernoulli.dev"))
+  :maintainer
+  '("Jonas Bernoulli" . "emacs.bicycle@jonas.bernoulli.dev")
+  :keywords
+  '("outlines")
+  :url "https://github.com/tarsius/bicycle")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
