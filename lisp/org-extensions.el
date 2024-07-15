493e9025e7aeb74ec90dff2b594c158b6c5f660d
Donald Brady <164683436+donaldbrady3v@users.noreply.github.com>
2024-07-14 18:49:08 -0700

    not using ivy so don't need this

--- lisp/org-extensions.el
+++ lisp/org-extensions.el
@@ -23,31 +23,3 @@
           (org-global-tags-completion-table)))
 
 
-;; should not need this but can't remember function
-(defun db/counsel-org-agenda-insert-link-to-headlines ()
-  "Insert a link to headers of `org-mode' files in the agenda."
-  (interactive)
-  (require 'org)
-  (let ((minibuffer-allow-text-properties t))
-    (ivy-read "Org headline: "
-              (counsel-org-agenda-headlines--candidates)
-              :action #'link/counsel-org-agenda-headlines-action-insert-link
-              :history 'counsel-org-agenda-headlines-history
-              :caller 'link/counsel-org-agenda-insert-link-to-headlines)))
-(defun link/counsel-org-agenda-headlines-action-insert-link (headline)
-  "Insert a link to the `org-mode' agenda HEADLINE."
-  (let ((link (save-window-excursion
-                (save-excursion
-                  (save-restriction
-		    (find-file (nth 1 headline))
-		    (goto-char (nth 2 headline))
-                    (org-store-link nil))))))
-    (insert link)))
-(ivy-set-actions
- 'counsel-org-agenda-headlines
- '(("i" link/counsel-org-agenda-headlines-action-insert-link "insert link to headline")
-   ("g" counsel-org-agenda-headlines-action-goto "goto headline")))
-(ivy-set-actions
- 'link/counsel-org-agenda-insert-link-to-headlines
- '(("i" link/counsel-org-agenda-headlines-action-insert-link "insert link to headline")
-   ("g" counsel-org-agenda-headlines-action-goto "goto headline")))
